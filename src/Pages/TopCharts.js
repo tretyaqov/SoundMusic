094e6bdbd0bc8d6bdbdcf1b6af6e11405aa31922
tima140802 <tima140802@mail.ru>
2023-11-07 21:33:10 +0600

    комит на цсс и жс у чартов, для влада(и не только)

--- src/Pages/TopCharts.js
+++ src/Pages/TopCharts.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './TopCharts.css'
 import moty_img from '../Components/Pictures/ava.png' /*импорт картинки исполнителя года*/
 import star_img from '../Components/Pictures/star.png' /*импорт картинки звёздочки*/
+import motm_img from '../Components/Pictures/oleg.png' /*импорт олега*/
 
 export default class TopCharts extends Component {
     render() {
@@ -24,11 +25,11 @@ export default class TopCharts extends Component {
                         <p className="string_counter">"10 409 011"</p>
                     </div>
                 </div>
-                <div className="row_motm">
-                    <p className="string_motm">Исполнители месяца</p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+                <div className="string_colums">
+                    <p className="string_motm ">Исполнители месяца</p> 
+                    <p className="string_sotm ">Песни месяца</p> 
+                    <p className="string_aotm">Альбомы месяца</p>
+                    <div className="line"></div> 
                 </div>
                 <div className="row_sotm">
                     
